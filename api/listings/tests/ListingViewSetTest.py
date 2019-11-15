7a18482959569ec9259a69542bf9831df8fa5fe5
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 10:01:23 -0800

    dev - ListingViewSetTest skeleton'd

--- api/listings/tests/ListingViewSetTest.py
+++ api/listings/tests/ListingViewSetTest.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Doing BDD this project. I'll come back & build some real tests later.
+class ListingViewSetTest(TestCase):
+
+    def Test_Get_All(self):
+        self.assertTrue(True, 'You haven\'t configured tests yet.')
