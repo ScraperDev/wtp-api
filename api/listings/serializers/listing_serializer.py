1bae0f63556b04f8cf266d699ae0b4a864cbea5c
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 10:57:06 -0800

    BF - fixed missing serializer fields

--- api/listings/serializers/listing_serializer.py
+++ api/listings/serializers/listing_serializer.py
@@ -1,8 +1,13 @@
 from listings.models.listing_model import Listing
-from rest_framework.serializers import ModelSerializer, IntegerField
+from rest_framework.serializers import ModelSerializer, IntegerField, BooleanField, CharField
 
 class ListingSerializer(ModelSerializer):
     asking_price = IntegerField()
+    volume = IntegerField()
+    partial_listing = BooleanField()
+    minimum_increment = IntegerField()
+    water_type = CharField()
+    
     class Meta:
         model = Listing
         fields = ('created_date', 'owner', 'asking_price', 'volume', 'partial_listing', 'minimum_increment', 'water_type', 'twai_confirmed', 'active')
