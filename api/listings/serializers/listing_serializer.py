35a4a0327a79829a046add63c6afb6a912913f22
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 10:08:33 -0800

    BF - fixed permission import

--- api/listings/serializers/listing_serializer.py
+++ api/listings/serializers/listing_serializer.py
@@ -1,4 +1,4 @@
-from listings.models.listing import Listing
+from listings.models.listing_model import Listing
 from rest_framework.serializers import ModelSerializer
 
 class ListingSerializer(ModelSerializer):
--- api/listings/views/listing_view_set.py
+++ api/listings/views/listing_view_set.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from listings.serializers import ListingSerializer
-from listings.models.isting import Listing
-from listings.permissions import IsOwnerOrReadOnly
+from listings.serializers.listing_serializer import ListingSerializer
+from listings.models.listing_model import Listing
+from listings.permissions.owner_permission import IsOwnerOrReadOnly
 
 # Create your views here.
 class ListingViewSet(ModelViewSet):
