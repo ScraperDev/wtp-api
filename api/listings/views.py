172a4ae3917c9a62d0f16c7a1c688247e4572723
Frederick Johnson <fritzj96@gmail.com>
2019-11-14 23:23:27 -0800

    building out the first ViewSet

--- api/listings/views.py
+++ api/listings/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .serializers import ListingSerializer
+from .models.listing import Listing
 
 # Create your views here.
+class ListingViewSet(ModelViewSet):
+    """
+    Read-Only Viewset for getting all or one Listing.
+    Authed users only.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    
