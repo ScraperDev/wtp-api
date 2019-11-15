d4b92c74ac2c09fd17d1d79fbefe9ea70d5eae0c
Frederick Johnson <fritzj96@gmail.com>
2019-11-14 23:13:04 -0800

    added ListingSerializer meta class inside

--- api/listings/serializers.py
+++ api/listings/serializers.py
@@ -2,3 +2,6 @@ from .models.listing import Listing
 from rest_framework.serializers import ModelSerializer
 
 class ListingSerializer(ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ['created_date', 'owner', 'asking_price', 'volume', 'partial_listing', 'minimum_increment', 'water_type', 'twai_confirmed', 'active']
