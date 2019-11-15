88eb4208de9793c254fb2cc6cbafbfe73ff452b5
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 09:54:07 -0800

    dev - set up urlconfig & make views dir into a module

--- api/listings/urls.py
+++ api/listings/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
+from listings.views.ListingViewSet import ListingViewSet
 
-urlpatterns = [
-    path('', )
-]
+listing_list = ListingViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+listing_detail = ListingViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+})
+
+urlpatterns = format_suffix_patterns([
+    path('listings/', listing_list, name='listing-list'),
+    path('listings/<int:pk>/', listing_detail, name='listing-detail'),
+])
