67c83f207213aa2c37be26323d0b13e4ff2d19c6
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 10:52:33 -0800

    added perform_create on save method for serializing the owner of an object

--- api/listings/serializers/listing_serializer.py
+++ api/listings/serializers/listing_serializer.py
@@ -1,7 +1,8 @@
 from listings.models.listing_model import Listing
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, IntegerField
 
 class ListingSerializer(ModelSerializer):
+    asking_price = IntegerField()
     class Meta:
         model = Listing
         fields = ('created_date', 'owner', 'asking_price', 'volume', 'partial_listing', 'minimum_increment', 'water_type', 'twai_confirmed', 'active')
--- api/listings/views/listing_view_set.py
+++ api/listings/views/listing_view_set.py
@@ -15,3 +15,5 @@ class ListingViewSet(ModelViewSet):
     serializer_class = ListingSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
