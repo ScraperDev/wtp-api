3d26ddf3b1ec2e7c39bf93e84ce6ead432cdfe3d
Frederick Johnson <fritzj96@gmail.com>
2019-11-15 15:57:03 -0800

    added docstrings & set_inactive to listing view set

--- api/listings/views/listing_view_set.py
+++ api/listings/views/listing_view_set.py
@@ -1,6 +1,7 @@
-from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from listings.serializers.listing_serializer import ListingSerializer
 from listings.models.listing_model import Listing
@@ -9,12 +10,26 @@ from listings.permissions.owner_permission import IsOwnerOrReadOnly
 # Create your views here.
 class ListingViewSet(RetrieveModelMixin, ListModelMixin, CreateModelMixin, GenericViewSet):
     """
-    Read-Only Viewset for getting all or one Listing.
-    Authed users only.
+    Retrieves, Lists, Creates, or Partially Updates a model.
     """
     queryset = Listing.objects.filter(active=True)
     serializer_class = ListingSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     
+    # Overrides the perform_create method in the Create Mixin
     def perform_create(self, serializer):
+        """
+        Overrides the perform_create method in the create mixin.
+        This is done to add the "owner" field.
+        """
         serializer.save(owner=self.request.user)
+
+    def set_inactive(self, request, *args, **kwargs):
+        """
+        Sets the active property to false. Effectively a "DELETE"
+        on the object from end-user standpoint.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data={ 'active': False }, partial=True)
+        serializer.save()
+        return Response(serializer.data)
