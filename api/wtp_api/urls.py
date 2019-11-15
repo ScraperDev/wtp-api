3613788934c71e5e023f709a8140d4cbef83cbdb
Frederick Johnson <fritzj96@gmail.com>
2019-11-14 19:52:50 -0800

    swapped the token views to use sliding jwts

--- api/wtp_api/urls.py
+++ api/wtp_api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainSlidingView, TokenRefreshSlidingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/listings/', include('listings.urls')),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/refresh/', TokenRefreshView.as_view()),
+    path('api/token/', TokenObtainSlidingView.as_view(), name="token_obtain"),
+    path('api/refresh/', TokenRefreshSlidingView.as_view(), name="token_refresh"),
 ]
