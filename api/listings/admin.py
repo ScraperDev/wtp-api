e1a82a418f5af62b9c5782771924dea84646150c
Frederick Johnson <fritzj96@gmail.com>
2019-11-21 10:37:13 -0800

    created offer model

--- api/listings/admin.py
+++ api/listings/admin.py
@@ -1,4 +1,14 @@
-from django.contrib import admin
+from django.contrib.admin import register, site, ModelAdmin, TabularInline
 from listings.models.listing_model import Listing
-# Register your models here.
-admin.site.register(Listing)
+from listings.models.offer_model import Offer
+
+site.register(Offer)
+
+class InlineOfferAdmin(TabularInline):
+    model = Offer
+
+@register(Listing)
+class ListingAdmin(ModelAdmin):
+    inlines = [
+        InlineOfferAdmin
+    ]
