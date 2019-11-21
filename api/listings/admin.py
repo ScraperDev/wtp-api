8c767ea6944ced17034a4720c8c5873bb362ba36
Frederick Johnson <fritzj96@gmail.com>
2019-11-21 10:59:09 -0800

    setting up admin to use Offer

--- api/listings/admin.py
+++ api/listings/admin.py
@@ -2,13 +2,16 @@ from django.contrib.admin import register, site, ModelAdmin, TabularInline
 from listings.models.listing_model import Listing
 from listings.models.offer_model import Offer
 
-site.register(Offer)
-
 class InlineOfferAdmin(TabularInline):
     model = Offer
 
 @register(Listing)
 class ListingAdmin(ModelAdmin):
+    readonly_fields = ('owner', 'asking_price', 'volume', 'water_type', 'partial_listing', 'minimum_increment')
     inlines = [
         InlineOfferAdmin
     ]
+
+@register(Offer)
+class OfferAdmin(ModelAdmin):
+    readonly_fields = ('listing', 'offer_price', 'requested_volume', 'owner')
