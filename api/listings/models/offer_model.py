b9b632b9c53f7fe280af4175fbf9e141e6169b1f
Frederick Johnson <fritzj96@gmail.com>
2019-11-21 10:21:42 -0800

    added created_date to Offer model

--- api/listings/models/offer_model.py
+++ api/listings/models/offer_model.py
@@ -1,15 +1,15 @@
-from django.db.models import Model, IntegerField, ForeignKey, CASCADE
+from django.db.models import Model, IntegerField, ForeignKey, CASCADE, DateTimeField
 
 class Offer(Model):
     """
     ### Offer Model
     #### Fields
-    - created_date: 
+    - created_date: DateTime when the offer was made.
     - offering_on: Listing that the offer is being made on
     - offer_price: Amount being offered, in $ per Acre Foot
     - requested_volume: How much of the water the offerer wants, in Acre Feet
     """
-
+    created_date = DateTimeField(auto_now_add=True)
     offering_on = ForeignKey('listings.Listing', on_delete=CASCADE, related_name="offers")
     offer_price = IntegerField()
     requested_volume = IntegerField()
